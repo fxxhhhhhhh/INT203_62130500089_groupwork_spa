52f76734d985e738b7c75e2dfac9124fa18af1c3
Supanan Chotteratat <supanan.c@mail.kmutt.ac.th>
2021-04-19 13:21:46 +0700

    62130500089 Add Content.vue

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,29 +1,13 @@
 <template>
   <div class="h-screen w-2/3 ml-auto mr-auto">
     <form @submit.prevent="checkForm">
-      <div class="grid grid-cols-3 gap-2 mt-6 ml-20 -mx-10">
-        <input
-          type="text"
-          class="input"
-          id="enterSong"
-          placeholder="Enter your favourite song"
-          v-model="enterSong"
-        />
-        <input
-          type="text"
-          class="input"
-          id="enterArtist"
-          placeholder="Enter artist's name"
-          v-model="enterArtist"
-        />
-        <button
-          type="submit"
-          class="w-24 bg-bluelogo text-white hover:bg-classicblue"
-        >
-          Submit
-        </button>
+      <div class="ml-auto mr-auto w-2/3 grid grid-cols-2 mt-6 gap-2">
         <p v-if="errors" class="text-red-600 text-left ml-2">*This fill can't be blank!</p>
         <p v-if="errors" class="text-red-600 text-left ml-2">*This fill can't be blank!</p>
+        <input type="text" class="input" id="enterSong" placeholder="Enter your favourite song" v-model="enterSong"/>
+        <input type="text" class="input" id="enterArtist" placeholder="Enter artist's name" v-model="enterArtist"/>
+        <button type="submit" class="h-8 justify-self-end w-24 bg-bluelogo text-white border-4 border-bluelogo hover:bg-classicblue hover:border-bluelogo">Submit</button>
+        <button @click="closeContent" class="h-8 justify-slef-start w-24 bg-classicblue text-white border-4 border-classicblue hover:bg-bluelogo hover:border hover:border-classicblue">Cancel</button>
       </div>
     </form>
     
@@ -43,7 +27,7 @@ const constraints = {
 export default {
     name: "Content",
     props: ["song", "artist"],
-    emits: ["enter-song"],
+    emits: ["submit-song", "close"],
     data() {
 		return {
 			enterSong: this.song,
@@ -51,7 +35,10 @@ export default {
 			errors: null,
 		};
 	},
-    methods: {
+  methods: {
+    closeContent() {
+      this.$emit("close", true);
+    },
 		checkForm() {
 			var validate = require("validate.js");
 			this.errors = validate(
@@ -65,16 +52,16 @@ export default {
 				console.log(this.errors);
 			} else {
 				this.submitForm();
-				alert("This song is already in your playlist. \nPlease enter another song.");
+				alert("This song has added to your playlist!");
 			}
 		},
-        submitForm() {
-			let songList = {
+    submitForm() {
+			let songLists = {
 				song: this.enterSong,
 				artist: this.enterArtist,
 			};
-			this.$emit("submit-song", songList);
+			this.$emit("submit-song", songLists);
 		},
 	},
-}
+};
 </script>
